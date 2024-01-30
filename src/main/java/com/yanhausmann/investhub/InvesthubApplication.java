549423a9160f318316c3389660faf3ec03e8a72e
hausmen <yanfhausmann@gmail.com>
2024-01-30 17:49:58 -0300

    Add initial project

---
+++ src/main/java/com/yanhausmann/investhub/InvesthubApplication.java
@@ -0,0 +1,13 @@
+package com.yanhausmann.investhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvesthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvesthubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yanhausmann/investhub/InvesthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanhausmann.investhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvesthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
